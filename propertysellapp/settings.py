2b02b91559c2f8e5a05b2d9dd5d698aa2a6635a9
hafis <hafis242@gmail.com>
2021-06-29 02:11:19 +0530

    fsh

--- propertysellapp/settings.py
+++ propertysellapp/settings.py
@@ -128,10 +128,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 # Stati
 # STATIC_ROOT=os.path.join(BASE_DIR,'images')
+# STATIC_URL = '/static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 django_heroku.settings(locals())
 # STATICFILES_DIRS=[
 #     os.path.join(BASE_DIR,'static')
 # ]
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 AUTH_USER_MODEL="ads.Userinfo"
